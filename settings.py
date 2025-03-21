26dac2e165fdc4e1eb8fef6f19b66f4a957c456d
aluuminite <reberwack@gmail.com>
2025-03-21 13:46:36 +0100

    Dynamic BG Color

--- settings.py
+++ settings.py
@@ -4,9 +4,8 @@ FPS = 60
 SCALE = 0.001  # 1 meter = 0.001 pixels (or 1 km = 1 pixel)
 
 
-# Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+# Color
+COLOR = (0, 0, 0)
 
 # Gravity Accuracy
 TIME_STEP = 0.5
