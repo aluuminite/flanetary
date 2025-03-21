44e1863891e90b0bbdf89e133fc05f48c1366dc1
aluuminite <reberwack@gmail.com>
2025-03-21 12:57:35 +0100

    Removed unnecessary gravity constant

--- settings.py
+++ settings.py
@@ -8,7 +8,5 @@ SCALE = 0.001  # 1 meter = 0.001 pixels (or 1 km = 1 pixel)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-# Gravitational constant (simplified for simulation purposes)
-G = 0.1
-
+# Gravity Accuracy
 TIME_STEP = 0.5
