d2b5cb44dde430409927b74effd78eef4baae0ef
aluuminite <reberwack@gmail.com>
2025-03-22 08:31:33 +0100

    Preparations for v0.2.0

--- settings.py
+++ settings.py
@@ -1,16 +1,10 @@
+# settings.py (before)
 SCREEN_WIDTH = 1600
 SCREEN_HEIGHT = 1200
 FPS = 60
-SCALE = 0.001  # 1 meter = 0.001 pixels (or 1 km = 1 pixel)
-
-
-# Color
-COLOR = (100, 0, 0)
+SCALE = 0.001
+COLOR = (0, 25, 0)
 WHITE = (255, 255, 255)
-
-# Gravity Accuracy
-TIME_STEP = 0.5
-G = 1
-
-# Globally toggles all logs
-LOG_TOGGLE = False  # Set to False to disable all logging
+TIME_STEP = 0.1
+G = 0.5
+LOG_TOGGLE = True
